33a97d738ec1ce3a9d42cee1a1d1203dd3868236
michep <mchepaykin@gmail.com>
2017-10-18 17:01:32 +0300

    start app as cluster

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import * as bodyParser from 'body-parser';
+import * as cluster from 'cluster';
+import { cpus } from 'os';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
-  app.use(bodyParser.json());
-  await app.listen(3000);
+const numCPUs = cpus().length;
+
+function bootstrap() {
+  if (cluster.isMaster) {
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+  } else {
+    const app = NestFactory.create(ApplicationModule)
+      .then(
+        (a) => {
+          a.use(bodyParser.json());
+          a.listen(3000);
+        },
+      );
+  }
 }
+
 bootstrap();
